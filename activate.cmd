56ff6e18616d703c02d389e46395924d0e19d55a
Andro Biert <androbiert@gmail.com>
2025-08-05 20:09:22 +0100

    Update project

--- activate.cmd
+++ activate.cmd
@@ -5,3 +5,12 @@ pip freeze > requirements.txt
 git init
 git add .
 git commit -m "Initial Django project commit"
+
+git remote add origin https://github.com/androbiert/miCarino.git
+git branch -M main
+git push -u origin main
+
+for any modifications, run:
+git add .
+git commit -m "Update project"
+git push
